09efc832cb4e3f71529b4d535d107fb798b145c2
Yash Jivani <yashjivani1999@gmail.com>
2024-05-29 02:30:45 +0000

    Update functions.py

--- functions.py
+++ functions.py
@@ -6,6 +6,10 @@ def get_weather(location, unit="fahrenheit"):
 
     import random
     # default weather info - can be changed to hit database/3-rd party APIs to get real-time weather info
+    # Workflow Steps
+    # <!-- - Fetch latitude and longitudes from [dataset](https://simplemaps.com/data/world-cities)  future scope--> 
+    # connect to sql database 
+
     weather_info = {"location": location, 
             "temperature": random.choice(range(50,90,1)), 
             "unit": unit, 
